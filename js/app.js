6bf28e31d1f4e2562b75a90930848ec060631ce2
ametelwokil2472 <ametelwokil@gmail.com>
2021-01-20 13:49:34 -0600

    I have made an edit to a file!

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let myNumber = 15
+if ( myNumber !== 'Number'){
+   console.log ('This is not a number')
+}else if (myNumber%3 == 0) {
+    console.log('Gen');
+}else if (myNumber%5 == 0) {
+    console.log('Buzz')
+}else if (myNumber%3 == 0 && myNumber%5 == 0){
+    console.log('GenBuzz ')
+}else {
+    console.log(myNumber)
+}
+
+let items = 'jeans'
+switch (items) {
+    case 'shoes':
+        console.log('Shoes are $50')
+        break;
+    case 'jeans':
+        console.log('Jeans- $25')
+        break
+    case 'hat':
+        console.log('Hat- $12')
+            break
+     case 'socks':
+        console.log('Socks- $2')   
+        break
+    default:'Invalid Item' 
+     
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  
+console.log(getRandomInt(50, 100));
