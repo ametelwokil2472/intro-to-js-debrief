5bddc181a6c6006a422949ddcf5788748dfb86d0
ametelwokil2472 <ametelwokil@gmail.com>
2021-01-20 14:03:23 -0600

    I have made an edit to a file!

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-let myNumber = 15
-if ( myNumber !== 'Number'){
+let myNumber = 'amet'
+if ( myNumber !== Number){
    console.log ('This is not a number')
-}else if (myNumber%3 == 0) {
+}else if (myNumber % 3 == 0) {
     console.log('Gen');
-}else if (myNumber%5 == 0) {
+}else if (myNumber % 5 == 0) {
     console.log('Buzz')
-}else if (myNumber%3 == 0 && myNumber%5 == 0){
+}else if (myNumber % 3 == 0 && myNumber % 5 == 0){
     console.log('GenBuzz ')
 }else {
     console.log(myNumber)
 }
 
-let items = 'jeans'
+let items = 'hat'
 switch (items) {
     case 'shoes':
         console.log('Shoes are $50')
